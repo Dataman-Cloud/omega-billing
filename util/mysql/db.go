77f1b83fda0c2111f17a3ec6577a5783d1e0b90d
yqguodataman <yqguo@dataman-inc.com>
2016-05-09 22:08:22 +0800

    update upgrade db

--- util/mysql/db.go
+++ util/mysql/db.go
@@ -43,11 +43,13 @@ func upgradeDB() {
 			log.Error("db err", err)
 		}
 		log.Error("can't upgrade db", errors)
-		log.Flush()
+		//log.Flush()
+		//panic(-1)
 	}
 	if !ok {
 		log.Error("can't upgrade db")
-		log.Flush()
+		//log.Flush()
+		//panic(-1)
 	}
 	log.Info("DB upgraded")
 	log.Flush()
