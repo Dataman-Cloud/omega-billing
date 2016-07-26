0ef1ed94f43da3ee9b54347f323215ebca286acc
lizw <zwli@dataman-inc.com>
2016-07-26 20:42:37 +0800

    status 401

---
+++ controller/billing_test.go
@@ -0,0 +1,53 @@
+package controller
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	baseUrl string
+	server  *httptest.Server
+)
+
+func TestMain(m *testing.M) {
+
+	server := startHttpServer()
+	baseUrl = server.URL
+	defer server.Close()
+	os.Exit(m.Run())
+}
+
+func auth(ctx *gin.Context) {
+	ctx.Set("uid", 123)
+	ctx.Next()
+}
+
+func startHttpServer() *httptest.Server {
+	router := gin.New()
+
+	v3 := router.Group("/api/v3", auth)
+	{
+		v3.GET("/billing", BillingList)
+
+	}
+
+	return httptest.NewServer(router)
+}
+
+func TestSearchIndexBadRequest(t *testing.T) {
+
+	req, err := http.NewRequest("GET", baseUrl+"/api/v3/billing", nil)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, resp.StatusCode, http.StatusBadRequest)
+}
